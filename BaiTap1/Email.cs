0fd9ab518915acd0baad6362ac4d0338e3b0dfef
Trần Mạnh Dũng <dungtmth1907043@fpt.edu.vn>
2020-09-08 16:34:30 +0700

    firt commit

---
+++ BaiTap1/Email.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1907Sem3.Models
+{
+    class Email
+    {
+        public string email { get; set; }
+        public string title {get; set;}
+        public string content { get; set; }
+
+        public Email(string email, string eitle, string content)
+        {
+            this.email = email;
+            this.title = title;
+            this.content = content;
+        }
+
+        public override string ToString()
+        {
+            return "Enail: " + email + "\n"
+                + "Title: " + title + "\n"
+                + "Content: " + content + "\n";
+        }
+    }
+}
